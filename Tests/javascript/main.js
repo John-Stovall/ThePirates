9c0f58cdae78dc578cba49f9a2e0d22f29f76b78
Robert Cordingly <robertcordingly@gmail.com>
2017-04-11 17:52:40 -0700

    Added the about page and input boxes for email and name.
    Basically just did everything he wants for the first iteration in the
    first 15 minutes of class. HTML makes this stuff ridiculously easy.

---
+++ Tests/javascript/main.js
@@ -0,0 +1,19 @@
+//This function is called when the page is first loaded.
+//Load any necessary information here.
+onload = function() {
+    updateHTML();
+};
+
+function submitEmail() {
+    localStorage["name"] = JSON.stringify(document.getElementById("name").value);
+    localStorage["email"] = JSON.stringify(document.getElementById("email").value);
+    updateHTML();
+}
+
+/**
+ * This function updates places in the html file so that it draws the proper information.
+ */
+function updateHTML() {
+    document.getElementById("displayName").innerHTML = localStorage["name"];
+    document.getElementById("displayEmail").innerHTML = localStorage["email"];
+}
