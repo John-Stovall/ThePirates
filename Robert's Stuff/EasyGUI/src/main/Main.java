25ae517cb7e27f7632b598e7936c2a116d037312
Robert Cordingly <robertcordingly@gmail.com>
2017-04-12 18:12:06 -0700

    Added a super basic user class.

--- Robert's Stuff/EasyGUI/src/main/Main.java
+++ Robert's Stuff/EasyGUI/src/main/Main.java
@@ -8,13 +8,57 @@ public class Main {
 
     public static void main(String[] args) {
 
+        User.getUsers().add(new User("Jim", "JimBob@gmail.com"));
+        User.getUsers().add(new User("Joe", "asdf@aol.com"));
+        User.getUsers().add(new User("John", "fdsa@msn.com"));
+        User.getUsers().add(new User("Jill", "123@comcast.net"));
+
+        start();
+        buildLogin();
+        buildRegister();
+        buildHome();
+        buildAbout();
+
+        //Set the current page:
+        GUI.window.gotoPage("login");
+    }
+
+    private static void start() {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
                 GUI.window.setTitle("DIY Program");
             }
         });
+    }
 
+    private static void buildLogin() {
+        GUI.window.gotoPage("login");
+        GUI.window.add(new GSpacer(25));
+        GUI.window.add(new GText("Select Account"));
+        GUI.window.add(new GSpacer(25));
+
+        for (User u : User.getUsers()) {
+            GButton button = new GButton(25, Color.blue, Color.red, u.getName() + ": " + u.getEmail()) {
+                @Override
+                public void clickAction() {
+                    GUI.window.gotoPage("home");
+                }
+            };
+            GUI.window.add(button);
+            GUI.window.add(new GSpacer(10));
+        }
+
+        GUI.window.add(new GSpacer(15));
+        GUI.window.add(new GButton(25, Color.blue, Color.red, "Add new Account") {
+            @Override
+            public void clickAction() {
+                GUI.window.gotoPage("register");
+            }
+        });
+    }
+
+    private static void buildRegister() {
         //Build the register page
         GUI.window.gotoPage("register");
         GUI.window.add(new GSpacer(25));
@@ -31,23 +75,25 @@ public class Main {
         GUI.window.add(new GButton(25, Color.blue, Color.red, "Submit") {
             @Override
             public void clickAction() {
-                GUI.window.gotoPage("home");
+                GUI.window.gotoPage("login");
             }
         });
+    }
 
+    private static void buildHome() {
         //Build the home page
         GUI.window.gotoPage("home");
         GUI.window.add(new GSpacer(25));
         GUI.window.add(new GText("Home"));
         GUI.window.add(new GSpacer(25));
-        GUI.window.add(new GText("There is nothing on this page yet."));
+        GUI.window.add(new GText("Hello"));
         GUI.window.add(new GSpacer(25));
         GUI.window.add(new GText("Register New Account:"));
         GUI.window.add(new GSpacer(5));
-        GUI.window.add(new GButton(25, Color.blue, Color.green,"Go Register") {
+        GUI.window.add(new GButton(25, Color.blue, Color.green,"Log Out") {
             @Override
             public void clickAction() {
-                GUI.window.gotoPage("register");
+                GUI.window.gotoPage("login");
             }
         });
         GUI.window.add(new GSpacer(5));
@@ -57,7 +103,9 @@ public class Main {
                 GUI.window.gotoPage("about");
             }
         });
+    }
 
+    private static void buildAbout() {
         //Build the about pages
         GUI.window.gotoPage("about");
         GUI.window.add(new GSpacer(25));
@@ -75,10 +123,6 @@ public class Main {
                 GUI.window.gotoPage("home");
             }
         });
-
-        //Set the current page:
-        GUI.window.gotoPage("register");
-
-
     }
+
 }
---
+++ Robert's Stuff/EasyGUI/src/main/User.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Bobby on 4/12/17.
+ */
+public class User {
+
+    private static ArrayList<User> allUsers = new ArrayList<>();
+
+    private static User loadedUser;
+
+    public static void setLoadedUser(User u) {
+        loadedUser = u;
+    }
+
+    public static User getLoadedUser() {
+        return loadedUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    public User(final String name, final String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public static ArrayList<User> getUsers() {
+        return allUsers;
+    }
+}
